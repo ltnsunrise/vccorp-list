1d1c70441ee389c6e27eb6814e45b29a024a0ae7
Admin <ltn.sunrise@gmail.com>
2020-08-24 10:26:13 +0700

    Updates

---
+++ precache-manifest.81ce9291fb95d7ec1bb78e7e2c244946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68d9417a8f732987cd3245524dcc893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd03864596f4afe379dd",
+    "url": "/static/css/main.bd723891.chunk.css"
+  },
+  {
+    "revision": "606fb1625a885a6d8021",
+    "url": "/static/js/2.6d724998.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.6d724998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd03864596f4afe379dd",
+    "url": "/static/js/main.01ef6c6f.chunk.js"
+  },
+  {
+    "revision": "11385006f93caa6b243a",
+    "url": "/static/js/runtime-main.666d07d1.js"
+  },
+  {
+    "revision": "f616e04c0de551656cc9bd34d19c02b3",
+    "url": "/static/media/artwork.f616e04c.svg"
+  }
+]);
